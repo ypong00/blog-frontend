b19fbd0b10c80b26a484995e157aed6083368624
ypong00 <ypong00@163.com>
2021-05-09 13:30:49 +0800

    更新

--- src/templates/Tag.vue
+++ src/templates/Tag.vue
@@ -1,7 +1,7 @@
 <template>
   <Layout>
     <!-- Page Header-->
-    <header class="masthead" style="background-image: url('img/home-bg.jpg')">
+    <header class="masthead" style="background-image: url('/img/home-bg.jpg')">
       <div class="overlay"></div>
       <div class="container">
         <div class="row">

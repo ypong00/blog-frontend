b772aa4834ddd993e5c5440ae13bff5a17f879b0
ypong00 <ypong00@163.com>
2021-05-10 21:33:29 +0800

    update

--- api/proxy.js
+++ api/proxy.js
@@ -6,7 +6,7 @@ module.exports = (req, res) => {
   
   // 处理代理目标地址
   if (req.url.includes('/uploads')) {
-      target = 'http://113.31.113.205:1337/'
+      target = 'http://113.31.113.205:1337'
   }
   
   // 创建代理对象并转发请求
@@ -17,7 +17,7 @@ module.exports = (req, res) => {
       // 通过路径重写，去除请求路径中的 /api
       //   例如 /api/boss/xxx 将被转发到 http://eduboss.lagou.com/boss/xxx
       //   例如 /api/front/xxx 将被转发到 http://eduboss.lagou.com/front/xxx
-      '^/uploads': ''
+      '^/uploads': '/uploads'
     }
   })(req, res)
 }
